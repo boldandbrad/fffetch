8c0bc3140c103b49baf75b70d8b6532e6ee2c414
boldandbrad <bradleycwojcik@gmail.com>
2025-02-05 20:22:52 -0500

    implement table ToMap and map ToTable

--- internal/util/structs.go
+++ internal/util/structs.go
@@ -5,3 +5,44 @@ type Table struct {
 	Headers []string
 	Rows    [][]string
 }
+
+type TableMap struct {
+	Name    string
+	headers []string
+	Dicts   []map[string]string
+}
+
+func (t Table) ToMap() TableMap {
+	var tableMap TableMap
+	tableMap.Name = t.Name
+	tableMap.headers = t.Headers
+	rowDicts := []map[string]string{}
+	for _, row := range t.Rows {
+		rowDict := map[string]string{}
+		for i, header := range t.Headers {
+			rowDict[header] = row[i]
+		}
+		rowDicts = append(rowDicts, rowDict)
+	}
+	tableMap.Dicts = rowDicts
+	return tableMap
+}
+
+func (m TableMap) ToTable() Table {
+	var table Table
+	table.Name = m.Name
+	table.Headers = m.headers
+	for _, dict := range m.Dicts {
+		row := []string{}
+		for _, header := range m.headers {
+			value, exists := dict[header]
+			if exists {
+				row = append(row, value)
+			} else {
+				row = append(row, "")
+			}
+		}
+		table.Rows = append(table.Rows, row)
+	}
+	return table
+}
